ae2250e0d0b64b5fc398fdff849aa5174b74e977
avargas <avargas@frontuari.com>
2017-05-14 15:48:34 -0400

    Clave admin cambiada

--- admin/backups/mappt_14052017.sql
+++ admin/backups/mappt_14052017.sql
@@ -1,24 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2017 a las 20:03:03
--- Versión del servidor: 10.1.16-MariaDB
--- Versión de PHP: 5.5.38
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `mappt`
---
 CREATE DATABASE IF NOT EXISTS `mappt` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `mappt`;
 
