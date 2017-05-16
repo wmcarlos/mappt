266d28feeb2b7a6fa8982c4ba9b8117d76b2ade6
avargas <avargas@frontuari.com>
2017-05-16 06:17:09 -0400

    Modificando libreria all transaccion para mejor manejo 2

--- admin/vistas/vistatsolicitud.php
+++ admin/vistas/vistatsolicitud.php
@@ -78,6 +78,46 @@ if(($operacion!='buscar' && $listo!=1) || ($operacion!='buscar' && $listo==1))
 
 
 
+<table id="transaccion2" class="transaccion_estile transaccion_mascara"   align="center" style="width: 80% !important;"> 
+	<caption>Transaccion de unidad de produccion</caption>
+	<!-- Cabecera de la tabla -->
+	<thead>
+		<tr>
+			<th>Nombre</th>
+			<th>Apellidos</th>
+			<th>Cedula</th>
+			<th>Hospital</th>
+			<th><input type="button" class="agregar" value="+"/></th>
+		</tr>
+	</thead>
+	<!-- Cuerpo de la tabla con los campos -->
+	<tbody>
+		<!-- fila base a tomar en cuenta-->
+		<tr class="tr_padre">
+			<td>
+				<input type="text" name="nombres[]"  />	
+			</td>
+			<td><input type="text"  name="apellidos[]"/></td>
+			<td><input type="text" name="cedulas[]"/></td>
+			<td>
+				<select name="hopitales[]" >
+					<option value="">Ingrese el hospital</option>
+					<option value="1">San Carlos</option>
+					<option value="2">HPO</option>
+					<option value="3">Bicentenaria</option>
+					<option value="4">Santa Maria</option>
+				</select>
+			</td>
+			<td class="eliminar"><input type="button" value="-"></td>
+		</tr>
+		<!-- fin de código: fila base --> 
+	</tbody>
+</table>
+
+<input type="button" name="" value="mi transaccion oculta" class="button-all-transaccion" tagmask="transaccion2">
+
+
+
 
 <?php @include('despues_form.php'); ?>
 </body>
